d6718712f9346e8dd046bb85a5ed564dc201e843
mateusfreireds <50429062+mateusfreireds@users.noreply.github.com>
2025-03-24 11:11:02 -0300

    Add files via upload

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,6 +4,15 @@
 #define TAMANHO_TABULEIRO 10
 #define TAMANHO_NAVIO 3
 #define QTDE_NAVIO 3
+#define TAMANHO_HABILIDADEX 5
+#define TAMANHO_HABILIDADEY 3
+
+// Definição dos tipos de habilidades
+#define CONE 1
+#define CRUZ 2
+#define OCTAEDRO 3
+
+
 
 
 
@@ -23,42 +32,42 @@ int checarNavioTabuleiro(int navio[3], char tabuleiro[TAMANHO_TABULEIRO][TAMANHO
     }
     if (navio[2] == 1){
         for (int i = 0; i < TAMANHO_NAVIO; i++) {
-            if (tabuleiro[navio[0]+i][navio[1]] == 'N') {
+            if (tabuleiro[navio[0]+i][navio[1]] == '3') {
                 return 1;
             }
         }
     }
     else if (navio[2] == 2){
         for (int i = 0; i < TAMANHO_NAVIO; i++) {
-            if (tabuleiro[navio[0]][navio[1]+i] == 'N') {
+            if (tabuleiro[navio[0]][navio[1]+i] == '3') {
                 return 1;
             }
         }
     }
     else if (navio[2] == 3){
         for (int i = 0; i < TAMANHO_NAVIO; i++) {
-            if (tabuleiro[navio[0]+i][navio[1]+i] == 'N') {
+            if (tabuleiro[navio[0]+i][navio[1]+i] == '3') {
                 return 1;
             }
         }
     }
     else if (navio[2] == 4){
         for (int i = 0; i < TAMANHO_NAVIO; i++) {
-            if (tabuleiro[navio[0]-i][navio[1]+i] == 'N') {
+            if (tabuleiro[navio[0]-i][navio[1]+i] == '3') {
                 return 1;
             }
         }
     }
     else if (navio[2] == 5){
         for (int i = 0; i < TAMANHO_NAVIO; i++) {
-            if (tabuleiro[navio[0]+i][navio[1]-i] == 'N') {
+            if (tabuleiro[navio[0]+i][navio[1]-i] == '3') {
                 return 1;
             }
         }
     }
     else if (navio[2] == 6){
         for (int i = 0; i < TAMANHO_NAVIO; i++) {
-            if (tabuleiro[navio[0]-i][navio[1]-i] == 'N') {
+            if (tabuleiro[navio[0]-i][navio[1]-i] == '3') {
                 return 1;
             }
         }
@@ -86,34 +95,45 @@ void posicionarNavio(char tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
         switch(navio[2]){ 
             case 1 :{
                 for (int i = 0; i < TAMANHO_NAVIO; i++) {
-                    tabuleiro[navio[0]+i][navio[1]] = 'N';  
+                    tabuleiro[navio[0]+i][navio[1]] = '3';  
+                
                     }
+            break;
 
             }
             case 2 :{
                 for (int i = 0; i < TAMANHO_NAVIO; i++) {
-                    tabuleiro[navio[0]][navio[1]+i] = 'N';  
+                    tabuleiro[navio[0]][navio[1]+i] = '3';  
+            
                     }
+            break;
             }
             case 3 :{
                 for (int i = 0; i < TAMANHO_NAVIO; i++) {
-                    tabuleiro[navio[0]+i][navio[1]+i] = 'N';  
+                    tabuleiro[navio[0]+i][navio[1]+i] = '3';  
                 }
+              break;
             }
             case 4 :{
                 for (int i = 0; i < TAMANHO_NAVIO; i++) {
-                    tabuleiro[navio[0]-i][navio[1]+i] = 'N';  
+                    tabuleiro[navio[0]-i][navio[1]+i] = '3';  
+            
                     }
+            break;
         }
             case 5 :{
                 for (int i = 0; i < TAMANHO_NAVIO; i++) {
-                    tabuleiro[navio[0]+i][navio[1]-i] = 'N';  
+                    tabuleiro[navio[0]+i][navio[1]-i] = '3';  
+            
                 }
+            break;
             }
             case 6 :{
                 for (int i = 0; i < TAMANHO_NAVIO; i++) {
-                    tabuleiro[navio[0]-i][navio[1]-i] = 'N';  
+                    tabuleiro[navio[0]-i][navio[1]-i] = '3';  
+            
                     }
+            break;
             }
         }
     }
@@ -136,6 +156,70 @@ void displayTabuleiro(char tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
     }
 }
 
+
+//Função para habilidade em cone
+void habilidadeCone(char tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    // Matriz que define o padrão do cone
+    char padraoCone[3][5] = {
+        {'0', '0', '5', '0', '0'},
+        {'0', '5', '5', '5', '0'},
+        {'5', '5', '5', '5', '5'}
+    };
+    
+    // Aplicar o padrão do cone no tabuleiro
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 5; j++) {
+            if(4 + i < TAMANHO_TABULEIRO && 4 + j < TAMANHO_TABULEIRO) {
+                if(padraoCone[i][j] == '5') {
+                    tabuleiro[4 + i][4 + j] = '5'; // Marca área afetada pela habilidade
+                }
+            }
+        }
+    }
+}
+
+
+void habilidadeCruz(char tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    // Matriz que define o padrão da cruz
+    char padraoCruz[3][5] = {
+        {'0', '0', '5', '0', '0'},
+        {'5', '5', '5', '5', '5'},
+        {'0', '0', '5', '0', '0'}
+    };
+    
+    // Aplicar o padrão da cruz no tabuleiro
+    for(int i2 = 0; i2 < 3; i2++) {
+        for(int j2 = 0; j2 < 5; j2++) {
+            if(4 + i2 < TAMANHO_TABULEIRO && 4 + j2 < TAMANHO_TABULEIRO) {
+                if(padraoCruz[i2][j2] == '5') {
+                    tabuleiro[6 + i2][1 + j2] = '5'; // Marca área afetada pela habilidade
+                }
+            }
+        }
+    }
+}
+
+void habilidadeOctaedro(char tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    // Matriz que define o padrão do Octaedro
+    char padraoCruz[3][5] = {
+        {'0', '0', '5', '0', '0'},
+        {'0', '5', '5', '5', '0'},
+        {'0', '0', '5', '0', '0'}
+    };
+    
+    // Aplicar o padrão do octaedro no tabuleiro
+    for(int i2 = 0; i2 < 3; i2++) {
+        for(int j2 = 0; j2 < 5; j2++) {
+            if(4 + i2 < TAMANHO_TABULEIRO && 4 + j2 < TAMANHO_TABULEIRO) {
+                if(padraoCruz[i2][j2] == '5') {
+                    tabuleiro[2 + i2][5 + j2] = '5'; // Marca área afetada pela habilidade
+                }
+            }
+        }
+    }
+}
+
+
 int main() {
     char tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
     inicializarTabuleiro(tabuleiro);
@@ -144,7 +228,9 @@ int main() {
     // Posicionando os navios no tabuleiro
     posicionarNavio(tabuleiro);
     
-
+    habilidadeCone(tabuleiro);
+    habilidadeCruz(tabuleiro);
+    habilidadeOctaedro(tabuleiro);
     // Exibindo o tabuleiro
     printf("Tabuleiro da Batalha Naval:\n");
     displayTabuleiro(tabuleiro);
